ea11bab7942ce86b94ae208f879f59f84eeb586d
lanwe <forgives@foxmail.com>
2014-10-16 19:45:10 +0800

    add Number scale.

--- src/main/java/com/forg/java/core/NumberKest.java
+++ src/main/java/com/forg/java/core/NumberKest.java
@@ -1,13 +1,17 @@
 package com.forg.java.core;
 
+import java.math.BigDecimal;
+
+
 public class NumberKest {
 
 	public static void main(String[] args) {
 		NumberKest k = new NumberKest();
 		String a = "1.00";
-		String b = "2.01";
+		String b = "211.11";
 		
-		System.out.println("" + k.add(convToDouble(a),convToDouble(b)));
+		//System.out.println("" + k.add(convToDouble(a),convToDouble(b)));
+		System.out.println(double2Str3(convToDouble(b)));
 		
 	}
 	
@@ -28,6 +32,27 @@ public class NumberKest {
         }
         return dblValue;
     }
-	
+    
+    
+    public static String double2Str(double pValue) {
+
+    	Double d = pValue * 0.2;
+    	return d.toString();
+    }
+    
+    public static String double2Str2(double pValue) {
+    	
+    	BigDecimal d = new BigDecimal(pValue);
+    	d = d.multiply(new BigDecimal(0.2));
+    	
+    	return d.toString();
+    }
+    
+    public static String double2Str3(double pValue) {
+    	
+    	Double amountMoney = pValue * 0.002;
+    	BigDecimal d = new BigDecimal(amountMoney);
+    	return d.setScale(2,java.math.BigDecimal.ROUND_HALF_UP).doubleValue() +"" ;
+    }
 
 }
